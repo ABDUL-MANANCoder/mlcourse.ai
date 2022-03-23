1719e8b73ca80adebc4a00375f27e98394c735ac
Daniil Chernyshov <chernyshoff.dp@gmail.com>
2022-03-23 23:54:52 +0300

    добавил 2 задание по 3 занятию #1

---
+++ lesson_3/task_2/task_2.py
@@ -0,0 +1,35 @@
+results = []
+
+with open("input_file.txt", "r") as f:
+	for line in f:
+		a, b, c = line.split()
+
+		b = int(b)
+		c = int(c)
+
+		if a == "+":
+			results.append(b + c)
+		elif a == "-":
+			results.append(b - c)
+		elif a == "*":
+			results.append(b * c)
+		elif a == "//":
+			results.append(b // c)
+		elif a == "%":
+			results.append(b % c)
+		elif a == "**":
+			results.append(b ** c)
+
+last_result = results[-1]
+
+with open("output_file.txt", "w") as file:
+	for result in results:
+		if result == last_result:
+			file.write(str(result))
+		else:
+			file.write(str(result) + ', ')
+
+
+with open("output.txt", "w") as file:
+	for i in range(5):
+		file.write(str(i))
